6cd7f4625f9f0400699c3319bb8ba062feaa73af
Kris Zyp <kriszyp@gmail.com>
2010-04-22 13:38:42 -0600

    Fixed test

--- test/test-load.js
+++ test/test-load.js
@@ -1,3 +1,4 @@
-require("../lib/nodules").ensure("http://github.com/kriszyp/web-modules/raw/master/lib/fs-promise", function(require){
-	var validate = require("./fs-promise");
+require("../lib/nodules").ensure("http://github.com/kriszyp/nodules/raw/master/lib/nodules-utils/fs-promise.js", function(require){
+        var validate = require("./fs-promise");
+        require("sys").puts("loaded fs-promise: "+ validate);
 });
